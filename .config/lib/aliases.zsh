fa15f12bfd10cc500ce8a64b246465c9982a80b5
TheGoldenR <gsadpais@proton.me>
2022-11-27 00:54:42 +0000

    feat: aliases; fix: scripts

--- .config/lib/aliases.zsh
+++ .config/lib/aliases.zsh
@@ -8,32 +8,27 @@ _exists(){
     command -v $1 > /dev/null 2>$1
 }
 
-# Go to the /home/$USER (~) directory, clears the terminal window and executes paleofetch
+# Go to the /home/$USER (~) directory, clears the terminal window 
 alias q="~ && clear && paleofetch"
+alias cl="clear && paleofetch"
 
 # Folders Shortcuts
 [ -d ~/Downloads ]            && alias dl='cd ~/Downloads'
 [ -d ~/Desktop ]              && alias dt='cd ~/Desktop'
-[ -d ~/Docs/github ]          && alias gh='cd ~/Docs/github'  
+[ -d ~/Docs/github ]          && alias ggh='cd ~/Docs/github'  
+[ -d ~/personal ]             && alias p='cd ~/personal'
+[ -d ~/work ]                 && alias p='cd ~/work'
 [ -d ~/.config ]              && alias dotf='cd ~/.config'
 
 # nvim 
-alias nvimr='nvim -R'
-#alias nv='nvim'
-
-# Spotifyd + Spotify-tui
-alias spt='gospt'
+alias n='nvim'
+alias nr='nvim -R'
 
 # ls
 alias ls='ls --color=auto'
 alias ll='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
-
-# cat with highlighting
-if _exists bat; then
-    alias cat='bat'
-fi
 
 # Show $PATH in readable view
 alias path='echo -e ${PATH//:/\\n}'
