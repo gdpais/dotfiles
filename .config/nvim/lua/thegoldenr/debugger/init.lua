6104f187e50f6ca5d5c1f9cc88b44a1722ad8b97
TheGoldenR <gsadpais@proton.me>
2022-12-03 17:30:32 +0000

    feat: dap

---
+++ .config/nvim/lua/thegoldenr/debugger/init.lua
@@ -0,0 +1,63 @@
+-- TODO: configurations + refact
+local ok, dap = pcall(require, "dap")
+if not ok then
+    return
+end
+
+local nmap = require("airborne.keymap").nmap
+
+-- fancy fancy <3 ty tj 
+vim.fn.sign_define("DapBreakpoint", { text = "ß", texthl = "", linehl = "", numhl = "" })
+vim.fn.sign_define("DapBreakpointCondition", { text = "ü", texthl = "", linehl = "", numhl = "" })
+vim.fn.sign_define("DapStopped", { text = "ඞ", texthl = "Error" })
+
+require("nvim-dap-virtual-text").setup()
+
+local dap_ui = require "dapui"
+local _ = dap_ui.setup {
+  layouts = {
+    {
+      elements = {
+        "scopes",
+        "breakpoints",
+        "stacks",
+        "watches",
+      },
+      size = 40,
+      position = "left",
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 10,
+      position = "bottom",
+    },
+  },
+}
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dap_ui.open(1)
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dap_ui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dap_ui.close()
+end
+
+nmap { "<F4>", function()
+    dap_ui.toggle(1)
+end }
+nmap { "<F5>", function()
+    dap_ui.toggle(2)
+end }
+nmap { "<Up>",  dap.continue }
+nmap { "<Down>", dap.step_over }
+nmap { "<Right>", dap.step_into }
+nmap { "<Left>", dap.step_out }
+nmap { "<Leader>b", dap.toggle_breakpoint }
+nmap { "<Leader>B", function()
+    dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+end }
--- .config/nvim/lua/thegoldenr/packer.lua
+++ .config/nvim/lua/thegoldenr/packer.lua
@@ -2,37 +2,59 @@ return require('packer').startup(function(use)
     use("wbthomason/packer.nvim")
 
     --All the stuff
-    use("nvim-lua/plenary.nvim")
-    use("nvim-lua/popup.nvim")
-    use("nvim-lualine/lualine.nvim")
-    use("nvim-telescope/telescope.nvim")
+    use "nvim-lua/plenary.nvim"
+    use "nvim-lua/popup.nvim"
+    use "nvim-lualine/lualine.nvim"
+    use "nvim-telescope/telescope.nvim"
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use("ThePrimeagen/harpoon")
+    use "ThePrimeagen/harpoon"
 
     --Completion
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-nvim-lua")
-    use("hrsh7th/cmp-nvim-lsp")
+    use "neovim/nvim-lspconfig"
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-nvim-lua"
+    use "hrsh7th/cmp-nvim-lsp"
     use { "tzachar/cmp-tabnine",  run = "./install.sh" , requires = "hrsh7th/nvim-cmp"}
-    use("L3MON4D3/LuaSnip")
-    use("saadparwaiz1/cmp_luasnip")
-    use("onsails/lspkind-nvim")
-    use("simrat39/symbols-outline.nvim")
 
-    use("TimUntersberger/neogit")
-    use("ThePrimeagen/git-worktree.nvim")
-    use("sindrets/diffview.nvim")
+--    should try for some time
+--    use {
+--        "zbirenbaum/copilot.lua",
+--        config = function()
+--        require("copilot").setup()
+--      end,
+--    }
+--    use {
+--      "zbirenbaum/copilot-cmp",
+--      after = { "copilot.lua" },
+--      config = function()
+--        require("copilot_cmp").setup()
+--      end,
+--    }
+
+    use "L3MON4D3/LuaSnip"
+    use "onsails/lspkind-nvim"
+    use "saadparwaiz1/cmp_luasnip"
+    use "tamago324/cmp-zsh"
+    use "simrat39/symbols-outline.nvim"
+
+    use "TimUntersberger/neogit"
+    use "ThePrimeagen/git-worktree.nvim"
+    use "sindrets/diffview.nvim"
 
     --Highlighting
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-    --use('nvim-treesitter/playground') -- Change treesitter colors
+    --use "nvim-treesitter/playground" -- Change treesitter colors
     use { "norcalli/nvim-colorizer.lua" , config = function () require'colorizer'.setup() end }
 
     --Colorscheme
-    use("folke/tokyonight.nvim")
+    use "folke/tokyonight.nvim"
+
+    -- Debug adapter protocol
+    use "mfussenegger/nvim-dap"
+    use "rcarriga/nvim-dap-ui"
+    use "theHamsta/nvim-dap-virtual-text"
 
     --Markdown
     use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
