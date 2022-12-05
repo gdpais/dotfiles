2c7cde687b77167837eb91b24c714a1223ecdce1
TheGoldenR <gsadpais@proton.me>
2022-12-05 21:05:27 +0000

    fix: harpoon prev/next

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,13 +1,19 @@
-local nmap = require("thegoldenr.keymap").nmap
+local ok, _ = pcall(require, "harpoon")
+
+if not ok then
+    return
+end
+
+local nmap = require("airborne.keymap").nmap
 local silent = { silent = true }
 
 nmap { "<leader>a", function() require("harpoon.mark").add_file() end, silent }
 nmap { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, silent }
 
-nmap { "<A-h>", function() require("harpoon.ui").nav_file(1) end, silent }
-nmap { "<A-j>", function() require("harpoon.ui").nav_file(2) end, silent }
-nmap { "<A-k>", function() require("harpoon.ui").nav_file(3) end, silent }
-nmap { "<A-l>", function() require("harpoon.ui").nav_file(4) end, silent }
+nmap { "<C-h>", function() require("harpoon.ui").nav_file(1) end, silent }
+nmap { "<C-j>", function() require("harpoon.ui").nav_file(2) end, silent }
+nmap { "<C-k>", function() require("harpoon.ui").nav_file(3) end, silent }
+nmap { "<C-l>", function() require("harpoon.ui").nav_file(4) end, silent }
 
-nmap { "<A-,>", function() require("harpoon.ui").nav_prev() end, silent }
-nmap { "<A-.>", function() require("harpoon.ui").nav_next() end, silent }
+nmap { "<C-[>", function() require("harpoon.ui").nav_prev() end, silent }
+nmap { "<C-]>", function() require("harpoon.ui").nav_next() end, silent }
