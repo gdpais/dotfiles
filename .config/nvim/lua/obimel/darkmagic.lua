62a4f03faacda4dd660ef0fcca135daeb75988fc
TheObimel <theobimel@gmail.com>
2022-12-18 22:16:52 +0000

    feat: darkmagic remove trailing lines

--- .config/nvim/lua/obimel/darkmagic.lua
+++ .config/nvim/lua/obimel/darkmagic.lua
@@ -3,8 +3,16 @@ local autocmd = vim.api.nvim_create_autocmd
 
 local darkMagic = augroup("DarkMagic", {})
 
+-- remove extra spaces
 autocmd("BufWritePre", {
     group = darkMagic,
     pattern = "*",
     command = [[%s/\s\+$//e]]
+})
+
+-- remove trailing lines
+autocmd("BufWritePre", {
+    group = darkMagic,
+    pattern = "*",
+    command = [[%s/$\n\+\%$//e]]
 })
